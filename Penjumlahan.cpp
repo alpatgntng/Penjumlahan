5e208174c913416d3c992d702a5b0117af8be1e7
alpatgntng <145646298+alpatgntng@users.noreply.github.com>
2023-09-20 18:51:39 -1200

    Create Penjumlahan.cpp

---
+++ Penjumlahan.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+    int a, b, c;
+    cout<< "Program Penjumlahan \n";
+     cout<<"Masukkan Angka pertama: ";
+     cin>>a;
+     cout<<"Masukan Angka kedua: ";
+     cin>>b;
+     cout<<"Hasil penjumlahan adalah: "<<a+b<<endl;
+     cout<<"Hasil pengurangan adalah: "<<a- b<<endl;
+     cout<<"Hasil perkalian adalah: "<<a*b<<endl;
+     cout<<"Hasil pembagian adalah: "<<a/b<<endl;
+     cout<<"Hasil sisa bagi adalah: "<<a % b<<endl;
+     return 0;
+}
